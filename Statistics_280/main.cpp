5f61303195b9d36401752fb4687e08b7428bbd9f
brianjernigan <brobertjernigan@gmail.com>
2023-05-07 16:50:41 -0400

    Initial Commit

---
+++ Statistics_280/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Statistics_280
+//
+//  Created by Brian Jernigan on 5/7/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
